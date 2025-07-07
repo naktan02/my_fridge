157dd4259337928a5b23c6554622698b774b0da7
naktan02 <jmgjmg102@gmail.com>
2025-07-07 18:28:49 +0900

    주석제거

--- api/v1/routes/dishes.py
+++ api/v1/routes/dishes.py
@@ -13,12 +13,10 @@ def get_repo(db: Session = Depends(get_db)) -> DishTypeRepository:
 
 @router.post("", response_model=schemas.DishType)
 def create_dish_type(
-    dish_create: schemas.DishTypeCreate, # 이 변수 이름은 그대로 둡니다.
+    dish_create: schemas.DishTypeCreate, 
     repo: DishTypeRepository = Depends(get_repo)
 ):
-    # repo.create() 를 호출할 때 사용하는 키워드를 수정합니다.
-    # dish_create=  ->  dish_type_create=
-    return repo.create(dish_type_create=dish_create) # 이 부분을 수정하세요.
+    return repo.create(dish_type_create=dish_create) 
 
 @router.get("", response_model=List[schemas.DishType])
 def get_all_dish_types(repo: DishTypeRepository = Depends(get_repo)):
